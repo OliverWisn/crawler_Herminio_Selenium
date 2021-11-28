89653fb3b740175583ea8bcea50d2b7615536e2c
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-11-28 16:13:05 +0100

    Check if the entire page in the script loadedcrawler_her_sel.py has been loaded
    Programm check if the entire page in the script
    loadedcrawler_her_sel.py has been loaded. The program waits up to
    10 seconds for the selected element to be loaded.

--- crawler_her_sel.py
+++ crawler_her_sel.py
@@ -5,21 +5,29 @@ import time
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
+my_url = "https://www.flashscore.com/"
 options = Options()
 options.add_argument("--headless")
 driver = Firefox(options=options)
-driver.get("https://www.flashscore.com/")
+driver.get(my_url)
 # Wait for page to fully render
-time.sleep(5)
-pageSource = driver.page_source
-bsObj = BeautifulSoup(pageSource, "html.parser")
-games = bsObj.find_all("div", {"class":\
-    "event__participant event__participant--home"})
-# print(len(games))
+# time.sleep(5)
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(By.CLASS_NAME, "tv-ico.icon.icon--tv"))
+finally:
+    pageSource = driver.page_source
+    bsObj = BeautifulSoup(pageSource, "html.parser")
+    games = bsObj.find_all("div", {"class":\
+        "event__participant event__participant--home"})
+    # print(len(games))
 
-for game in games:
-    print(game.get_text())
+    for game in games:
+        print(game.get_text())
 
-driver.quit()
+    driver.quit()
