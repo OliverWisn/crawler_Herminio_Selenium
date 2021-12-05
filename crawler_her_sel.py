db2aca95205d845e1266f04c0d7dda5c7d1dd90f
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-12-05 10:42:55 +0100

    Determine the number of matches displayed.
    The change in the code scrap the current number of matches displayed
    on the website and save this value in a variable.

--- crawler_her_sel.py
+++ crawler_her_sel.py
@@ -1,14 +1,12 @@
 # crawler_her_sel.py
 # -*- coding: utf-8 -*-
 
-import time
-
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+import pandas as pd
 
 my_url = "https://www.flashscore.com/"
 options = Options()
@@ -17,16 +15,24 @@ driver = Firefox(options=options)
 driver.get(my_url)
 # Wait for page to fully render
 try:
-    element = WebDriverWait(driver, 10).until(
+    element = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.ID, "box-over-content-a")))
 finally:
     pageSource = driver.page_source
-    bsObj = BeautifulSoup(pageSource, "html.parser")
-    games = bsObj.find_all("div", {"class":\
-        "event__participant event__participant--home"})
-    # print(len(games))
+    soccer = driver.find_element(By.XPATH , "/html/body/div[5]/div/div[1]/a[1]")
+    matches_soccer = soccer.get_attribute("data-sport-count")
+    print(matches_soccer)
 
-    for game in games:
-        print(game.get_text())
+# /html/body/div[5]/div/div[1]/a[1]
+
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[3]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[4]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[3]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[4]
+
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[5]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[6]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[5]
+# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[6]
 
     driver.close()
