014b0d4e93ae9cc29888185adcaafc1e98afa67a
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-12-07 13:45:03 +0100

    Organize the code according to the rules of art
    Organizing the code according to the rules of the programming in
    python.

--- crawler_her_sel.py
+++ crawler_her_sel.py
@@ -39,72 +39,61 @@ finally:
     # the attribute.
     soccer = driver.find_element(By.XPATH , "/html/body/div[5]/div/div[1]/a[1]")
     matches_soccer = soccer.get_attribute("data-sport-count")
-    print(matches_soccer)
 
     countries = driver.find_elements(By.CLASS_NAME , "event__title--type")
-    print(len(countries))
-
-#    leagues = driver.find_elements(By.CLASS_NAME , "event__title--name")
-#    print(len(leagues))
 
     sum_to_iterate = int(matches_soccer) + len(countries)
-    print(sum_to_iterate)
-
-#    iframe = driver.switch_to.frame("lsadvert-zid-396-iframe")
-#    home_team_x = driver.find_element(By.XPATH , "/html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[2]/div[3]").text
-#    home_team_y = driver.find_element(By.CLASS_NAME , "event__participant event__participant--home")
-#    home_teams_z = driver.find_elements(By.XPATH , './/div[@class="sportName soccer"]/div[2]/div[3]')
-#    .//table[@class='table-main detail-odds sortable']/tbody/tr/td[2]/div
-#    print(home_team_x)
-#    print(home_team_y)
-#    print(len(home_teams_z))
-#    for game in home_teams_z:
-#        print(game.text)
     
     for ind in range(1, (sum_to_iterate + 1)):
         try:
-            country = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[1]/div/span[1]').text
+            country = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+\
+             ']/div[1]/div/span[1]').text
             list_of_countries.append(country)
         except:
             country = ""
             list_of_countries.append(country)
 
         try:
-            league = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[1]/div/span[2]').text
+            league = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+\
+             ']/div[1]/div/span[2]').text
             list_of_leagues.append(league)
         except:
             league = ""
             list_of_leagues.append(league)
 
         try:
-            home_team = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[3]').text
+            home_team = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+']/div[3]').text
             list_of_home_teams.append(home_team)
         except:
             home_team = ""
             list_of_home_teams.append(home_team)
 
         try:
-            score_for_home_team = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[5]').text
+            score_for_home_team = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+']/div[5]').text
             list_of_scores_for_home.append(score_for_home_team)
         except:
             score_for_home_team = ""
             list_of_scores_for_home.append(score_for_home_team)
 
         try: 
-            score_for_away_team = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[6]').text
+            score_for_away_team = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+']/div[6]').text
             list_of_scores_for_away.append(score_for_away_team)
         except:
             score_for_away_team = ""
             list_of_scores_for_away.append(score_for_away_team)
 
         try:
-            away_team = driver.find_element(By.XPATH , './/div[@class="sportName soccer"]/div['+str(ind)+']/div[4]').text
+            away_team = driver.find_element(By.XPATH ,\
+             '//div[@class="sportName soccer"]/div['+str(ind)+']/div[4]').text
             list_of_away_teams.append(away_team)
         except:
             away_team = ""
             list_of_away_teams.append(away_team)
-
-        print(away_team)
 
     dictionary_of_matches["countries"] = list_of_countries
     dictionary_of_matches["leagues"] = list_of_leagues
@@ -112,30 +101,5 @@ finally:
     dictionary_of_matches["scores_for_home_teams"] = list_of_scores_for_home
     dictionary_of_matches["scores_for_away_teams"] = list_of_scores_for_away
     dictionary_of_matches["away_teams"] = list_of_away_teams
-    print(len(list_of_away_teams))
-
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[1]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[2]/div[1]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[2]/div[2]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[2]/div[3]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[197]/div[3]
-
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[1]/div[1]/div/span[1]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[3]/div[1]/div/span[1]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[6]/div[1]/div/span[1]
-
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[1]/div[1]/div/span[2]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[3]/div[1]/div/span[2]
-
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[3]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[4]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[3]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[4]
-
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[5]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[10]/div[6]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[5]
-# /html/body/div[6]/div[1]/div/div[1]/div[2]/div[5]/div[2]/div/section/div/div/div[11]/div[6]
 
     driver.quit()
